f8c026cf5817c81211066d61e26ebe39fdfc6abe
nagi91204 <nonaka912.kdgo.student@gmail.com>
2026-02-12 10:11:34 +0900

    ログイン機能追加

--- attendance/attendance/models.py
+++ attendance/attendance/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    id = models.IntegerField(primary_key=True)
+
+    name = models.CharField(max_length=100)
+
+    email = models.EmailField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    elective_subjects = models.ManyToManyField(Subject, blank=True)
+
+    def __str__(self):
+        return self.name
